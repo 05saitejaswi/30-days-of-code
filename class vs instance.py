0334a800d494c68d7d3fba2140af0d5ce3122fda
chakravaram.saitejaswi <50818486+05saitejaswi@users.noreply.github.com>
2020-01-10 12:59:58 +0530

    class vs instance
    class vs instance

---
+++ class vs instance.py
@@ -0,0 +1,15 @@
+class Person:
+    def __init__(self,initialAge):
+        self.age=initialAge
+    def amIOld(self): 
+        if self.age<13:
+            if self.age<0:
+                self.age=0
+                print('Age is not valid, setting age to 0.')
+            print('You are young.')
+        elif 13<=self.age<18:
+            print('You are a teenager.')
+        else:
+            print('You are old.')
+    def yearPasses(self): 
+        self.age+=1
