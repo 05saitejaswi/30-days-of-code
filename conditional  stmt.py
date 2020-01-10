352bb1572422bb490965503b169f2fb1185397f5
chakravaram.saitejaswi <50818486+05saitejaswi@users.noreply.github.com>
2020-01-10 12:34:36 +0530

    conditional  stmt
    conditional  stmt

---
+++ conditional  stmt.py
@@ -0,0 +1,20 @@
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+   N=int(input())
+if N%2 == 1 or (N > 5 and N < 21):
+    print("Weird")
+else:
+    print("Not Weird")
+    
+    input:
+    3
+    output:
+    Weird
