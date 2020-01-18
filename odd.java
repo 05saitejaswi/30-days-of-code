04a879cb01fd169b86b941cf41b599782ac9fa04
chakravaram.saitejaswi <50818486+05saitejaswi@users.noreply.github.com>
2020-01-19 01:21:07 +0530

    if else
    if else

---
+++ odd.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();            
+        String ans="";
+
+        if(n>=1 && n<=100){
+            if(n%2==1){
+                ans = "Weird";
+            }
+            else{
+                if(n>=2 && n<=5 ){
+                   ans= "Not Weird";
+                }
+                else if (n>=6 && n<=20){
+                    ans = "Weird";
+                }
+                else if(n>20) {
+                    ans = "Not Weird";
+                }
+            }
+        }
+        else{
+            ans="Invalid no! Enter a number between 1 and 100.";
+        }
+
+        System.out.println(ans);
+
+    }
+}
